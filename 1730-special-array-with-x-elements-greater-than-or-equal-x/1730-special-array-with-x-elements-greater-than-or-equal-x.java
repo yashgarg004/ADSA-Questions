6679bec875d8ccfdb2b625f406a713f259acbd57
Yash Garg <ygarg7298@gmail.com>
2025-10-14 19:29:37 +0530

    Time: 3 ms (12.41%), Space: 41.1 MB (97.36%) - LeetHub

---
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int specialArray(int[] nums) {
+        Arrays.sort(nums); // Sort the array
+        int n = nums.length;
+        
+        for (int x = 0; x <= n; x++) {
+            int count = 0;
+            
+            for (int num : nums) {
+                if (num >= x) count++;
+            }
+            if (count == x) return x;
+        }
+        return -1;
+    }
+}
